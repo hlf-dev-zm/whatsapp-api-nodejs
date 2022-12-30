75eca9880ac77e5e050906f026f64611ca702aa6
Zainab Murtaza Husain <zainab@kmindz.in>
2022-12-30 12:24:04 +0530

    added pm2 confi file

---
+++ pm2.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'whatsapp-api',
+            script: './src/server.js',
+            log_date_format: 'YYYY-MM-DD HH:mm',
+        },
+    ],
+}
