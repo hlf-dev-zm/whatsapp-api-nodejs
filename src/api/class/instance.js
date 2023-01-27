048d28d6511314855cff789c20fec8b8f40e037e
Zainab Murtaza Husain <zainab@kmindz.in>
2023-01-27 12:11:28 +0530

    remove generateHighQualityLinkPreview

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -24,7 +24,6 @@ class WhatsAppInstance {
         logger: pino({
             level: config.log.level,
         }),
-        generateHighQualityLinkPreview: true,
     }
     key = ''
     authState
