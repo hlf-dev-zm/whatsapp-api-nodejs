aa0b514bead2a4cc94d1adb4c3539ed6d7b4a1ad
Zainab Murtaza Husain <zainab@kmindz.in>
2023-04-05 15:22:34 +0530

    Revert "console statements to debug"
    This reverts commit 24b1ba49426c9079b5c8b7cb22440babf32ef005.

--- src/api/controllers/instance.controller.js
+++ src/api/controllers/instance.controller.js
@@ -57,12 +57,10 @@ exports.qrbase64 = async (req, res) => {
 
 exports.info = async (req, res) => {
     const instance = WhatsAppInstances[req.query.key]
-    console.log(instance)
     let data
     try {
         data = await instance.getInstanceDetail(req.query.key)
     } catch (error) {
-        console.log(error)
         data = {}
     }
     return res.json({
