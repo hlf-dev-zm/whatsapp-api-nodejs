b03ff6cc3d8daf07a666d507301f9a12676a0fb4
Zainab Murtaza Husain <zainab@kmindz.in>
2023-04-07 14:44:35 +0530

    made changes to work synchronously

--- src/api/class/session.js
+++ src/api/class/session.js
@@ -23,45 +23,30 @@ class Session {
                 })
             }
 
-            allCollections.map(async (key) => {
-                console.log('Whatsapp Instances ', WhatsAppInstances)
+            for (var key of allCollections) {
                 if (!WhatsAppInstances[key]) {
-                    console.log('Found the instance ', key)
                     const query = {}
-                    console.log(await db.collection(key).find(query).toArray())
                     let docs = await db.collection(key).find(query).toArray()
                     if (docs.length > 0) {
-                        console.log('no error')
-                        const webhook = !config.webhookEnabled
-                            ? undefined
-                            : config.webhookEnabled
-                        const webhookUrl = !config.webhookUrl
-                            ? undefined
-                            : config.webhookUrl
+                        const webhook = config.webhookEnabled
+                        const webhookUrl = config.webhookUrl
                         const instance = new WhatsAppInstance(
                             key,
                             webhook,
                             webhookUrl
                         )
-                        console.log('Before init ')
                         await instance.init()
-                        console.log('Assigning instance ')
                         WhatsAppInstances[key] = instance
-                        console.log(
-                            'current whatsapp instance ',
-                            WhatsAppInstances
-                        )
                         restoredSessions.push(key)
                     }
                 } else {
                     restoredSessions.push(key)
                 }
-            })
+            }
         } catch (e) {
             logger.error('Error restoring sessions')
             logger.error(e)
         }
-        console.log('Whatsapp Instances', WhatsAppInstances)
         return restoredSessions
     }
 }
