d95e056e54246661cec91f46abaa78cf4cb67732
Zainab Murtaza Husain <zainab@kmindz.in>
2023-04-14 11:09:03 +0530

    minor changes

--- src/api/controllers/instance.controller.js
+++ src/api/controllers/instance.controller.js
@@ -1,6 +1,4 @@
 const { WhatsAppInstance } = require('../class/instance')
-const fs = require('fs')
-const path = require('path')
 const config = require('../../config/config')
 const { Session } = require('../class/session')
 
@@ -139,7 +137,7 @@ exports.list = async (req, res) => {
         WhatsAppInstances[key].getInstanceDetail(key)
     )
     let data = await Promise.all(instance)
-    
+
     return res.json({
         error: false,
         message: 'All instance listed',
